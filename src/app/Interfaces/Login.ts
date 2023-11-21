1e3c87b2f07212c5e2b160515f231374e82affc0
Rimbow002 <enzotrujilloacosta@gmail.com>
2023-11-20 21:38:50 -0500

    feat: localstorage added

---
+++ src/app/Interfaces/Login.ts
@@ -0,0 +1,14 @@
+export interface UserData  {
+  id: number;
+  name: string;
+  mail: string;
+  username: string;
+  password: string;
+  rolID: string;
+}
+export interface Credentials  {
+  username: string;
+  password: string;
+}
+
+
